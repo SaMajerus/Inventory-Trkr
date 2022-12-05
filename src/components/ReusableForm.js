d0275d206a2b030be378b651d22a6963c61582e8
Samuel Majerus <ladolego@gmail.com>
2022-12-05 10:48:40 -0800

    In a nutshell:  fixes the issue where the values of some/all the Sack's props would be erased if the User left some/all the input fields blank on the EditSackForm page.  [Hooks (from pre-Week 4 Lessons) are utilized so that the NewSackForm doesn't break.] [In each property's Hook, the ternary operator states that the stored value will be set to whatever is entered by the User if the Sack object already exists. Otherwise, the prop's initial value will be set to 'null'.   (The actual changing of the value is done in each Input field.   As an example: for the 'name' prop, see Lines 19-20.)] [The 'currSack' PropType passed in from the 'EditSackForm' component (Line 17 of that file, specifically) is critical to the operation described above, as it passes the existing Sack object into this component. This way, the given Sack's prop values are automatically pre-loaded in the Input fields, so that those that aren't edited won't be erased by accident.]

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,29 +1,47 @@
 import React from "react";
+import {useState} from "react";
 import PropTypes from "prop-types";
 
 function ReusableForm(props) {
+  //Hooks
+  const [name, setName] = useState(props.currSack ? props.currSack.name : null);
+  const [origin, setOrigin] = useState(props.currSack ? props.currSack.origin : null);
+  const [roast, setRoast] = useState(props.currSack ? props.currSack.roast : null);
+  const [price, setPrice] = useState(props.currSack ? props.currSack.price : null);
+  const [poundsLeft, setPoundsLeft] = useState(props.currSack ? props.currSack.poundsLeft : null);
+
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
         <input
           type='text'
           name='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)} 
           placeholder='Bean Name' />
         <input
           type='text'
           name='origin'
+          value={origin}
+          onChange={(e) => setOrigin(e.target.value)} 
           placeholder='Delivered from' />
         <input
           type='text'
           name='roast'
+          value={roast}
+          onChange={(e) => setRoast(e.target.value)}
           placeholder='Coffee Roast type' />
         <input
           type='number'
           name='price'
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
           placeholder='Price per LB' />
         <input
           type='number'
           name='poundsLeft'
+          value={poundsLeft}
+          onChange={(e) => setPoundsLeft(e.target.value)}
           placeholder='LBs remaining in sack' />
         <button type='submit'>{props.buttonText}</button>
       </form>
@@ -32,6 +50,7 @@ function ReusableForm(props) {
 }
 
 ReusableForm.propTypes = {
+  currSack: PropTypes.object,   //For 'EditSackForm'
   formSubmissionHandler: PropTypes.func,
   buttonText: PropTypes.string
 };
